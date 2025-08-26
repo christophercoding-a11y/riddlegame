e5fbd17d6ebec4b3ce9c27f6f64b61b7247bfd99
christophercodinga11y <christopherjack14@gmail.com>
2025-08-26 15:23:11 -0500

    convert functions to arrow functions

--- js/app.js
+++ js/app.js
@@ -33,16 +33,53 @@ const submitBtn = document.querySelector(".submit-btn");
 let currentRiddleIndex = 0;
 let score = 0;
 
-function showRiddle() {
+// function showRiddle() {
+//     const currentRiddle = riddleData[currentRiddleIndex];
+//     riddleElement.textContent = currentRiddle.riddle;
+//     optionsElements.forEach((option, index) => {
+//         option.textContent = currentRiddle.options[index];
+//         option.classList.remove("selected", "correct", "wrong");
+//     });
+// }
+
+const showRiddle = () => {
     const currentRiddle = riddleData[currentRiddleIndex];
-    riddleElement.textContent = currentRiddle.riddle;
-    optionsElements.forEach((option, index) => {
-        option.textContent = currentRiddle.options[index];
-        option.classList.remove("selected", "correct", "wrong");
-    });
+    optionsElements.forEach((option, index)=> {
+        option.textContent = currentRiddle.options[index]
+        option.classList.remove("selected", "correct", "wrong")
+    })
 }
 
-function handleNextRiddle() {
+// function handleNextRiddle() {
+//     const selectedOption = document.querySelector("li.option.selected");
+//     if (selectedOption) {
+//         const currentRiddle = riddleData[currentRiddleIndex];
+
+//         optionsElements.forEach(option => {
+//             if (option.textContent === currentRiddle.answer) {
+//                 option.classList.add("correct");
+//             } else if (option.classList.contains("selected")) {
+//                 option.classList.add("wrong");
+//             }
+//         });
+
+//         if (selectedOption.textContent === currentRiddle.answer) {
+//             score++;
+//         }
+
+//         setTimeout(() => {
+//             currentRiddleIndex++;
+//             if (currentRiddleIndex < riddleData.length) {
+//                 showRiddle();
+//                 resetOptions();
+//             } else {
+//                 showResults();
+//             }
+//         }, 1500);
+//     }
+// }
+
+const handleNextRiddle = () => {
     const selectedOption = document.querySelector("li.option.selected");
     if (selectedOption) {
         const currentRiddle = riddleData[currentRiddleIndex];
@@ -69,15 +106,33 @@ function handleNextRiddle() {
             }
         }, 1500);
     }
-}
+};
 
-function resetOptions() {
+// function resetOptions() {
+//     optionsElements.forEach(option => {
+//         option.classList.remove("selected", "correct", "wrong");
+//     });
+// }
+
+const resetOptions =()=> {
     optionsElements.forEach(option => {
-        option.classList.remove("selected", "correct", "wrong");
-    });
+        option.classList.remove("selected", "correct", "wrong")
+    })
 }
 
-function showResults() {
+// function showResults() {
+//     const riddleContainer = document.querySelector(".riddle-container");
+//     riddleContainer.innerHTML = `
+//         <h2>You solved ${score} out of ${riddleData.length} riddles</h2>
+//         <button class="restart-btn">Restart Game</button>
+//     `;
+
+//     document.querySelector(".restart-btn").addEventListener("click", () => {
+//         location.reload();
+//     });
+// }
+
+const showResults = () => {
     const riddleContainer = document.querySelector(".riddle-container");
     riddleContainer.innerHTML = `
         <h2>You solved ${score} out of ${riddleData.length} riddles</h2>
@@ -87,7 +142,7 @@ function showResults() {
     document.querySelector(".restart-btn").addEventListener("click", () => {
         location.reload();
     });
-}
+};
 
 optionsElements.forEach(option => {
     option.addEventListener("click", () => {
